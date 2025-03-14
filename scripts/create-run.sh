52af8aff495f79b28d57526791f612813b2a3a32
Laszlo Puskas <laszlo.puskas1@gmail.com>
2025-03-14 17:45:34 +0100

    fix bash linter errors, added golang linter config

--- scripts/create-run.sh
+++ scripts/create-run.sh
@@ -1,1 +1,3 @@
- curl -X POST -H "Content-Type: application/json" http://0.0.0.0:8000/runs -d @run.json | jq -r .run_id
+#!/usr/bin/env bash
+
+curl -X POST -H "Content-Type: application/json" http://0.0.0.0:8000/runs -d @run.json | jq -r .run_id
--- scripts/test-run.sh
+++ scripts/test-run.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 run_id=$(./create-run.sh)
-./get-run.sh $run_id
+./get-run.sh "$run_id"
