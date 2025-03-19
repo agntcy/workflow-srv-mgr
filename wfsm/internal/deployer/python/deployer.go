655f0f22c2f056874fc088cb1d37e9a3ad0ae6ff
Magyari Sandor Szilard <sancyx@gmail.com>
2025-03-19 17:58:42 +0100

    use docker compose

--- wfsm/internal/deployer/python/deployer.go
+++ wfsm/internal/deployer/python/deployer.go
@@ -5,20 +5,24 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"strconv"
 	"strings"
+
+	"github.com/compose-spec/compose-go/v2/types"
+	"github.com/docker/cli/cli/flags"
+	"github.com/docker/compose/v2/cmd/formatter"
+	"github.com/docker/compose/v2/pkg/api"
+	"github.com/docker/docker/api/types/container"
+	dockerClient "github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 
 	containerClient "github.com/cisco-eti/wfsm/internal/container_client"
 	"github.com/cisco-eti/wfsm/internal/deployer/python/source"
 	"github.com/cisco-eti/wfsm/internal/util"
 	"github.com/cisco-eti/wfsm/manifests"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/api/types/mount"
-	dockerClient "github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
-	"github.com/docker/go-connections/nat"
-	imagespecv1 "github.com/opencontainers/image-spec/specs-go/v1"
+	//imagespecv1 "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/docker/cli/cli/command"
 
+	"github.com/docker/compose/v2/pkg/compose"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 )
@@ -65,11 +69,24 @@ func (d *deployer) Deploy(ctx context.Context) error {
 	}
 
 	log.Debug().Msg(fmt.Sprintf("agent image: %s", imgNameWithTag))
-
+	//TODO should come from outside as a map
 	envVars, err := godotenv.Read(d.envFilePath)
 	if err != nil {
 		return fmt.Errorf("failed to get envVars: %v", err)
 	}
+	err = d.startContainer(ctx, envVars, imgNameWithTag, err, d.agManifest.Metadata.Ref.Name, log)
+	if err != nil {
+		return fmt.Errorf("failed to start agent: %v", err)
+	}
+	return nil
+}
+
+func (d *deployer) startContainer(ctx context.Context, envVars map[string]string,
+	imgName string, err error, serviceName string, log *zerolog.Logger) error {
+
+	ctx = log.With().Str("serviceName", serviceName).
+		Logger().WithContext(ctx)
+	log = zerolog.Ctx(ctx)
 
 	cli, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv, dockerClient.WithAPIVersionNegotiation())
 	if err != nil {
@@ -78,49 +95,24 @@ func (d *deployer) Deploy(ctx context.Context) error {
 	defer containerClient.Close(ctx, cli)
 
 	containerName := strings.Join([]string{"wfsm", d.agManifest.Metadata.Ref.Name}, "-")
-	containerID, err := containerClient.IsContainerRunning(ctx, cli, imgNameWithTag, containerName)
+	port, err := containerClient.IsContainerRunning(ctx, cli, imgName, containerName)
 	if err != nil {
 		return fmt.Errorf("failed to check if agent is running: %v", err)
 	}
-
-	if len(containerID) > 0 {
-		log.Info().Msg("agent is already running")
-
-		err := cli.ContainerStop(ctx, containerID, container.StopOptions{})
+	if port > 0 {
+		log.Info().Msg(fmt.Sprintf("agent is already using port: %d", port))
+	} else {
+		port, err = util.GetNextAvailablePort()
 		if err != nil {
-			return fmt.Errorf("failed to stop existing container_client: %v", err)
+			return fmt.Errorf("failed to get next available port: %v", err)
 		}
-
-		err = containerClient.RemoveContainer(ctx, cli, containerID)
-		if err != nil {
-			return fmt.Errorf("failed to remove existing container_client: %v", err)
-		}
+		log.Debug().Msgf("agent will listen on port: %d", port)
 	}
 
-	containerID, err = d.startContainer(ctx, envVars, imgNameWithTag, err, cli, containerName, log)
-	if err != nil {
-		return fmt.Errorf("failed to start container_client: %v", err)
-	}
-
-	rc, err := cli.ContainerLogs(ctx, containerID, container.LogsOptions{
-		ShowStdout: true,
-		ShowStderr: true,
-		Follow:     true,
-	})
-
-	if err != nil {
-		return fmt.Errorf("failed to get container_client logs: %v", err)
-	}
-	stdcopy.StdCopy(os.Stdout, os.Stderr, rc)
-
-	return nil
-}
-
-func (d *deployer) startContainer(ctx context.Context, envVars map[string]string,
-	imgName string, err error, cli *dockerClient.Client, containerName string, log *zerolog.Logger) (string, error) {
-
-	manifestPath := "/opt/manifest.yaml"
+	manifestPath := "/opt/storage/manifest.yaml"
 	envVars["AGENT_MANIFEST_PATH"] = manifestPath
+	envVars["API_HOST"] = "0.0.0.0"
+	envVars["API_PORT"] = "8000"
 	agentName := d.agManifest.Metadata.Ref.Name
 
 	if d.deployment.FrameworkConfig.LangGraphConfig != nil {
@@ -136,71 +128,128 @@ func (d *deployer) startContainer(ctx context.Context, envVars map[string]string
 		envVars["AGENTS_REF"] = fmt.Sprintf(`{"%s": "%s"}`, agentName, path)
 
 	} else {
-		return "", fmt.Errorf("unsupported framework config")
+		return fmt.Errorf("unsupported framework config")
 	}
 
-	port, err := util.GetNextAvailablePort()
-	if err != nil {
-		return "", fmt.Errorf("failed to get next available port: %v", err)
-	}
-	log.Debug().Msgf("Next available port: %d", port)
+	//platform := strings.Split(util.CurrentArchToDockerPlatform(), "/")
+	//dockerPlatform := &imagespecv1.Platform{OS: platform[0], Architecture: platform[1]}
 
-	platform := strings.Split(util.CurrentArchToDockerPlatform(), "/")
-	dockerPlatform := &imagespecv1.Platform{OS: platform[0], Architecture: platform[1]}
-
-	hostConfig := getHostConfig(SERVER_PORT, HOST_IP, strconv.Itoa(port))
 	if d.hostStorageFolder != "" {
-		hostConfig.Mounts = []mount.Mount{
-			{
-				Type:     mount.TypeBind,
-				Source:   d.hostStorageFolder,
-				Target:   "/opt/storage",
-				ReadOnly: false,
-			},
-		}
 		envVars["AGWS_STORAGE_FILE"] = path.Join("/opt/storage", fmt.Sprintf("agws_storage_%s.pkl", agentName))
 	}
-	containerConfig := getContainerConfig(imgName, envVars, SERVER_PORT)
-
-	containerID, err := containerClient.CreateContainer(ctx, cli, containerConfig, hostConfig, dockerPlatform, containerName)
-	if err != nil {
-		return "", fmt.Errorf("failed to create docker container_client: %v", err)
-	}
-
-	ctx = log.With().Str("container_id", containerID).
-		Logger().WithContext(ctx)
-	log = zerolog.Ctx(ctx)
 
 	manifestFileBuf, err := manifests.NewNullableAgentManifest(&d.agManifest).MarshalJSON()
 	if err != nil {
-		return "", fmt.Errorf("failed to marshal agent manifest: %v", err)
+		return fmt.Errorf("failed to marshal agent manifest: %v", err)
 	}
-	workspacePath, err := os.MkdirTemp("", "agent_run_")
+
+	err = os.WriteFile(path.Join(d.hostStorageFolder, "manifest.yaml"), manifestFileBuf, util.OwnerCanReadWrite)
 	if err != nil {
-		return "", fmt.Errorf("creating temporary workspace dir failed: %v", err)
+		return fmt.Errorf("failed to write manifest to temporary workspace dir: %v", err)
 	}
-	err = os.WriteFile(path.Join(workspacePath, "manifest.yaml"), manifestFileBuf, util.OwnerCanReadWrite)
+
+	project := &types.Project{
+		Name: "wfsm",
+	}
+
+	pc, err := types.ParsePortConfig(fmt.Sprintf("0.0.0.0:%v:%v", port, SERVER_PORT))
 	if err != nil {
-		return "", fmt.Errorf("failed to write manifest to temporary workspace dir: %v", err)
+		return fmt.Errorf("failed to parse port config: %v", err)
+	}
+	project.Services = make(map[string]types.ServiceConfig)
+	project.Services[serviceName] = types.ServiceConfig{
+		Name: serviceName,
+		Labels: map[string]string{
+			api.ProjectLabel: project.Name,
+			api.OneoffLabel:  "False",
+			api.ServiceLabel: serviceName,
+		},
+		//ContainerName: serviceName,
+		Image:       imgName,
+		Ports:       pc,
+		Environment: getEnvVars(envVars),
+		Volumes: []types.ServiceVolumeConfig{
+			{
+				Type:   "bind",
+				Source: d.hostStorageFolder,
+				Target: "/opt/storage",
+			},
+		},
 	}
 
-	err = containerClient.CopyToContainer(ctx, cli, containerID, workspacePath, "/opt")
+	// Use the MarshalYAML method to get YAML representation
+	projectYAML, err := project.MarshalYAML()
 	if err != nil {
-		return "", fmt.Errorf("failed to copy manifest to container_client: %w", err)
+		return err
 	}
+	log.Debug().Msg(string(projectYAML))
 
-	if err := os.RemoveAll(workspacePath); err != nil {
-		log.Error().Err(err).Str("path", workspacePath).Msg("failed to remove temporary dir")
-	}
-
-	err = containerClient.StartContainer(ctx, cli, containerID)
+	dockerCli, err := command.NewDockerCli(command.WithBaseContext(ctx))
 	if err != nil {
-		return "", fmt.Errorf("failed to start docker container_client: %v", err)
+		return fmt.Errorf("failed to create docker cli: %v", err)
+	}
+	clientOptions := flags.ClientOptions{
+		Hosts: []string{"unix:///var/run/docker.sock"},
+		//opts := &flags.ClientOptions{Hosts: []string{fmt.Sprintf("unix://%s", socket)}}
+		LogLevel:  "debug",
+		TLS:       false,
+		TLSVerify: false,
+	}
+	err = dockerCli.Initialize(&clientOptions)
+	if err != nil {
+		return fmt.Errorf("failed to initialize docker client: %v", err)
 	}
 
-	log.Info().Msg(fmt.Sprintf("agent running in container: %s, listening on port: %d", containerName, port))
+	backend := compose.NewComposeService(dockerCli) //.(commands.Backend)
+	err = backend.Up(ctx, project, api.UpOptions{
+		//Create: api.CreateOptions{
+		//	Services: project.ServiceNames(),
+		//},
+		//Start: api.StartOptions{
+		//	Services: project.ServiceNames(),
+		//	Project:  project,
+		//},
+	})
+	if err != nil {
+		return err
+	}
 
-	return containerID, nil
+	list, err := backend.Ps(ctx, project.Name, api.PsOptions{All: true})
+	if err != nil {
+		return err
+	}
+	for _, c := range list {
+		//log.Debug().Msg(fmt.Sprintf("%s %s %s %v", c.Name, c.ID, c.Status))
+		log.Info().Msg(fmt.Sprintf("agent running in container: %s, listening on port: %d status: %s", c.Name, port, c.Status))
+	}
+
+	logConsumer := formatter.NewLogConsumer(ctx, os.Stdout, os.Stderr, true, true, true)
+	err = backend.Logs(ctx, project.Name, logConsumer, api.LogOptions{
+		Project:  project,
+		Services: []string{serviceName},
+		Tail:     "100",
+		Follow:   true,
+	})
+	if err != nil {
+		return err
+	}
+
+	log.Info().Msg(fmt.Sprintf("agent running in container: %s, listening on port: %d", serviceName, port))
+
+	return nil
+}
+
+func getStringPtr(s string) *string {
+	return &s
+}
+
+func getEnvVars(envvars map[string]string) map[string]*string {
+	ev := make(map[string]*string)
+	for k, v := range envvars {
+		// clone the v value to avoid reference issues
+		ev[k] = getStringPtr(strings.Clone(v))
+	}
+	return ev
 }
 
 func getHostConfig(port nat.Port, hostIP string, hostPort string) *container.HostConfig {
