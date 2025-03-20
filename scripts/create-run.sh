c7dc898a92cdf8e1c47e6f29be0f1f5f986c6750
Magyari Sandor Szilard <sancyx@gmail.com>
2025-03-20 10:36:09 +0100

    change logs a bit

--- scripts/create-run.sh
+++ scripts/create-run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST -H "Content-Type: application/json" http://0.0.0.0:8000/runs -d @run.json | jq -r .run_id
+curl -X POST -H "Content-Type: application/json" http://127.0.0.1:$PORT/runs -d @run.json | jq -r .run_id
