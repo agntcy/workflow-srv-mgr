875079d8d427a3e2cc9b3cdab44a6e003806a455
Magyari Sandor Szilard <sancyx@gmail.com>
2025-03-24 22:37:36 +0100

    add test case for docker runner

--- wfsm/internal/wfsm/command/deploy.go
+++ wfsm/internal/wfsm/command/deploy.go
@@ -12,7 +12,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cisco-eti/wfsm/internal"
-	"github.com/cisco-eti/wfsm/internal/platforms/docker"
+	"github.com/cisco-eti/wfsm/internal/platforms/docker_compose"
 
 	"github.com/cisco-eti/wfsm/internal/builder"
 	"github.com/cisco-eti/wfsm/internal/util"
