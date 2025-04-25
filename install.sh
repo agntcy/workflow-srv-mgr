41525a31a64017f1c99320baebd61d8bea35bef1
Marco Trinelli (mtrinell) <mtrinell@cisco.com>
2025-04-25 11:24:35 +0200

    chore: update tag
    Signed-off-by: Marco Trinelli (mtrinell) <mtrinell@cisco.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-: ${WFSM_TAG:="0.1.0"}
+: ${WFSM_TAG:="0.1.1"}
 : ${WFSM_ARCH:=$(arch)}
 : ${WFSM_OS:=$(echo $(uname -s) | tr '[:upper:]' '[:lower:]')}
 : ${WFSM_TARGET:=${HOME}/.wfsm/bin}
